abb30080398ae21dc97be697c138ad00a5bd1c34
Mats Andresen <mandresen@papego.de>
2018-03-12 14:28:14 +0100

    remove unused compiler advises

--- src/Server.hs
+++ src/Server.hs
@@ -1,11 +1,4 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators #-}
 
 module Server ( app ) where
